f2dc72b9e8b4f9128820f12e13dd42b3d36c8b7c
Will Ballard <wballard@mailframe.net>
2013-10-23 11:56:03 -0500

    websocket echo, works under nginx

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var WebSocketServer = require('ws').Server;
 
 app.get('/.test', function (req, res) {
   res.sendfile(__dirname + '/index.html');
@@ -8,13 +8,17 @@ app.get('/.test', function (req, res) {
 app.get('/*', function(request, response) {
   response.end(request.path.slice(1));
 });
-io.sockets.on('connection', function (socket) {
-  console.log('connected')
-  socket.on('echo', function (data) {
-    console.log('echo', data);
-    socket.emit('echo', data);
+var port = process.env.PORT || 3000;
+
+
+server.listen(port);
+var wsserver = new WebSocketServer({server: server});
+wsserver.on('connection', function(socket){
+  socket.on('message', function(data){
+    console.log(data);
+    socket.send(data);
   });
 });
-var port = process.env.PORT || 3000;
-server.listen(port);
+
+
 console.log('Listening on ' + port);

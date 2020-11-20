20e182da33268a2c3dba60392cefd8f99ba6e5b3
Ian Groff <ian@iangroff.com>
2020-11-19 19:37:36 -0600

    better outputs

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var WebSocketServer = require('ws').Server;
 
+function handleRequest(request, response){
+  response.send(JSON.stringify(
+    {
+      params: request.params,
+      cookies: request.cookies,
+      hostname: request.hostname,
+      body: request.body,
+      query: request.query,
+      method: request.method,
+      protocol: request.protocol,
+      path: request.path,
+      environ: process.env,
+      version: process.version
+    }
+  )).end();
+}
+
 app.get('/.test', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
-app.get('/*', function(request, response) {
-  response.end(request.path.slice(1));
-});
+
+app.get('/*', handleRequest);
+app.post('/*', handleRequest);
+
 var port = process.env.PORT || 3000;
 
-
-server.listen(port);
 var wsserver = new WebSocketServer({server: server});
 wsserver.on('connection', function(socket){
   socket.on('message', function(data){
@@ -20,5 +36,5 @@ wsserver.on('connection', function(socket){
   });
 });
 
-
+server.listen(port);
 console.log('Listening on ' + port);
